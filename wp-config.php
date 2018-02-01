8bb89e38c507fe79374478111399c87f2982209f
LiliwoL Kimsufi <bitbucket@liliwol.fr>
2018-02-01 21:56:18 +0100

    wp-config

--- wp-config.php
+++ wp-config.php
@@ -29,26 +29,26 @@ define( 'DB_COLLATE', '' );
 // Salts, for security
 // Grab these from: https://api.wordpress.org/secret-key/1.1/salt
 // ==============================================================
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define( 'AUTH_KEY',         'wordpress_liliwol_skeleton' );
+define( 'SECURE_AUTH_KEY',  'wordpress_liliwol_skeleton' );
+define( 'LOGGED_IN_KEY',    'wordpress_liliwol_skeleton' );
+define( 'NONCE_KEY',        'wordpress_liliwol_skeleton' );
+define( 'AUTH_SALT',        'wordpress_liliwol_skeleton' );
+define( 'SECURE_AUTH_SALT', 'wordpress_liliwol_skeleton' );
+define( 'LOGGED_IN_SALT',   'wordpress_liliwol_skeleton' );
+define( 'NONCE_SALT',       'wordpress_liliwol_skeleton' );
 
 // ==============================================================
 // Table prefix
 // Change this if you have multiple installs in the same database
 // ==============================================================
-$table_prefix  = 'wp_';
+$table_prefix  = 'wls00_';
 
 // ================================
 // Language
 // Leave blank for American English
 // ================================
-define( 'WPLANG', '' );
+define( 'WPLANG', 'fr-FR' );
 
 // ===========
 // Hide errors
